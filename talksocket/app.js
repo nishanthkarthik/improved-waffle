b212b15747e357c22270d7265891df9e04ab08be
Karthik Nishanth <nishanthkarthik@live.com>
2016-02-21 21:33:02 +0530

    Login page done

--- talksocket/app.js
+++ talksocket/app.js
@@ -1,3 +1,4 @@
+/* global __dirname */
 var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
@@ -7,6 +8,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login')
 
 var app = express();
 
@@ -23,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/login', login);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
