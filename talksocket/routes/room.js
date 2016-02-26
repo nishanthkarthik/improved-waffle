4ed8f4ff31570adfe54ffae99757c3bd4db86315
nishanthkarthik <nishanthkarthik@live.com>
2016-02-26 20:09:08 +0530

    Add UI code

--- talksocket/routes/room.js
+++ talksocket/routes/room.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-    res.render('home', { title: 'Your chatroom' });
+    res.render('room', { title: 'talkSocket Chatroom' });
 });
 
 module.exports = router;
